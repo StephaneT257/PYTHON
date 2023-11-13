e0af3fb94ac809f996931d3006536647f78e5bd3
Stephane <st154@hw.ac.uk>
2023-11-13 21:02:08 +0000

    commit the dons

---
+++ list.py
@@ -0,0 +1,9 @@
+list = [1,2,3,4,5,6]
+
+list.remove(3)
+
+squaredlist = []
+
+for num in list:
+    squaredlist.append(num**2)
+    print(squaredlist)
---
+++ main.py
@@ -0,0 +1,20 @@
+class cars: 
+    def __init__(self, brand, engine) -> None:
+        self.wheels = 4
+        self.brand = brand
+        self.engine = engine 
+        
+    def wheelmulti(self):
+        self.wheels *= 2 
+
+brand = input("input car brand")
+engine = input("input car engine")
+
+car1 = cars(brand, engine)        
+
+car1.wheelmulti()
+
+print(f"your car is a  {car1.brand} it has {car1.wheels} wheels, and a {car1.engine} engine" )
+
+
+
