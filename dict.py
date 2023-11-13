b30c508ec8699f57a0281dcbd9e9450ef0ab470b
Stephane <st154@hw.ac.uk>
2023-11-13 21:24:53 +0000

    commiting dict.py

---
+++ dict.py
@@ -0,0 +1,28 @@
+dict = {
+    "bmw": "1series",
+    "audi": "a4",
+    "peugot": "206",
+
+}
+
+# print(dict["bmw"])
+
+
+
+Motorvehicles = [
+     {
+    "bmw": "1series",
+    "audi": "a4",
+    "peugot": "206",
+
+},
+
+     {
+    "yamaha": "bikes",
+    "audi": "a4",
+    "peugot": "206",
+
+}
+]
+
+print(Motorvehicles[0]["bmw"])
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ class cars:
     def wheelmulti(self):
         self.wheels *= 2 
 
-brand = input("input car brand")
-engine = input("input car engine")
+# brand = input("input car brand")
+# engine = input("input car engine")
 
-car1 = cars(brand, engine)        
+car1 = cars(brand ="peugot", engine= 4)        
 
 car1.wheelmulti()
 
-print(f"your car is a  {car1.brand} it has {car1.wheels} wheels, and a {car1.engine} engine" )
+print(f"your car is a  {car1.brand} it has {car1.wheels} wheels, and a {car1.engine}l engine" )
 
 
 
